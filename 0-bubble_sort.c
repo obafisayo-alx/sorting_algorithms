283a6ca071f962aa4b49eafff6805ff7fc53fc57
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-22 02:23:36 +0100

    modified sort.h with new prototypes and modified 0-bubble_sort file with answers

---
+++ 0-bubble_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+#include "stdbool.h"
+
+/**
+ * swap - swaps the two values, "left" and "right"
+ * 
+ * @left: the value on the left
+ * @right: the value on the right
+ * Return: void
+*/
+void swap(int *left, int *right)
+{
+	int temp;
+	temp = *left;
+	*left = *right;
+	*right = temp;
+}
+
+/**
+ * bubble_sort - swaps two contiguous element that satifies the comparison
+ * 
+ * @array: array to be sorted
+ * @size: number of elements in the array
+ * Return: void
+*/
+void bubble_sort(int *array, size_t size)
+{
+	bool swapped;
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				swapped = true;
+				print_array(array, size);
+			}
+		}
+		if (swapped == false)
+		{
+			break;
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -21,5 +21,6 @@ typedef struct listint_s
 /**Prototypes**/
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
 
 #endif /** _SORT_H_ **/
