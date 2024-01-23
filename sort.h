3d8aa9e27894380607660407c463a7d3db171ac1
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-23 17:00:37 +0100

    commit selection sort

--- sort.h
+++ sort.h
@@ -22,6 +22,8 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void swap(int *left, int *right);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif /** _SORT_H_ **/
